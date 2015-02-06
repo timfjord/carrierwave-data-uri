5f865a854c85979980a82fc59b17388f9e69e6d7
Tima Maslyuchenko <insside@gmail.com>
2015-02-06 19:24:40 +0200

    add specs

---
+++ spec/parser_spec.rb
@@ -0,0 +1,28 @@
+require 'spec_helper'
+
+RSpec.describe CarrierWave::DataUri::Parser do
+  let(:data_uri) do
+    'data:image/gif;base64,R0lGODlhAQABAJEAAAAAAP////8AAP///yH5BAEAAAMALAAAAAABAAEAAAICVAEAOw=='
+  end
+
+  describe 'initialization' do
+    it 'should raise error when data is invalid' do
+      expect { CarrierWave::DataUri::Parser.new 'invadli_data' }.to raise_error
+    end
+
+    it 'should parse data on init' do
+      parsed = CarrierWave::DataUri::Parser.new data_uri
+
+      expect(parsed.extension).to eql 'gif'
+    end
+  end
+
+  describe '#to_file' do
+    it 'should generate temp file based on data' do
+      parsed = CarrierWave::DataUri::Parser.new data_uri
+      file = parsed.to_file
+
+      expect(file).to be_a_kind_of Tempfile
+    end
+  end
+end
---
+++ spec/spec_helper.rb
@@ -0,0 +1,4 @@
+require 'bundler/setup'
+Bundler.setup
+
+require 'carrierwave-data-uri'
