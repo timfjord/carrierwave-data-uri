39edd492799de65cd38596c4465f6b734511d87c
kntmrkm <kntmrkm@gmail.com>
2016-09-27 00:49:45 +0900

    Add attr_accessor for data_uri_cache

--- lib/carrierwave_data_uri/mount.rb
+++ lib/carrierwave_data_uri/mount.rb
@@ -9,10 +9,12 @@ module CarrierWave
 
         class_eval <<-RUBY, __FILE__, __LINE__+1
           attr_accessor :#{column}_data_uri
+          attr_accessor :#{column}_data_uri_cache
           attr_accessor :#{column}_data_filename, :#{column}_data_mimetype
 
           def #{column}_data_uri=(data)
             return if data.blank?
+            self.#{column}_data_uri_cache = data
             self.#{column} = Parser.new(data).to_file original_filename: self.#{column}_data_filename, content_type: self.#{column}_data_mimetype
           end
         RUBY
